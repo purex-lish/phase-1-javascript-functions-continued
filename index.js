e8b3160d38bb966f749aabdbab958c68657104da
Purity Wambua <puritywamb@gmail.com>
2024-06-29 01:07:47 +0300

    Functions continued

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+     return `This Saturday, I want to ${activity}!`;
+    }
+    console.log(saturdayFun());
+
+
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+console.log(mondayWork());
+
+
+function wrapAdjective(flair = "|") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+
+const encouragingPromptFunction = wrapAdjective("||");
+console.log(encouragingPromptFunction("a dedicated programmer")); 
+
+
+const defaultPromptFunction = wrapAdjective("|");
+console.log(defaultPromptFunction("a hard worker")); 
+
+
